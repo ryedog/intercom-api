198b019e9c3111d0afb3492f5ca02d3fe9ecc6a8
Ryan Hubbard <ryanmhubbard@gmail.com>
2015-07-16 02:35:04 -0400

    adding /admins resource tests

---
+++ test/admins.spec.js
@@ -0,0 +1,23 @@
+describe('Admins Resource', function() {
+  var id = 109625;
+
+
+  describe('getAdmins()', function() {
+    before_promise(function() {
+      return intercom.getAdmins();
+    });
+    
+    it('returns list of admins', function() {
+      subject.should.have.property('admins');
+    });
+
+    it('should have at least 1 admin', function() {
+      subject.admins.should.have.length.above(0);
+    });
+
+    it('should have admin ' + id, function(){
+      subject.admins[0].should.have.property('id', id);
+    });
+  });
+
+});
